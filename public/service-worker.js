5fbc411ea967d9caa9da36d7602e548ae3d29607
Alexandra Murawski <almurawski15@gmail.com>
2021-05-27 12:45:30 -0400

    edited install even listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,16 +8,20 @@ const FILES_TO_CACHE = [
 
 ];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const CACHE_NAME = 'static-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v2';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
+      .open(CACHE_NAME)
+      .then((cache => {
+        console.log("Your files have been pre-cached!");
+        return cache.addAll(FILES_TO_CACHE));
+      })
   );
+
+  self.skipWaiting();
 });
 
 // The activate handler takes care of cleaning up old caches.
