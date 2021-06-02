7ec9ba82f5b0ae090c17b2dc00bfe9aa0fa97b55
Alexandra Murawski <almurawski15@gmail.com>
2021-06-01 23:01:21 -0400

    deploying

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,7 +9,7 @@ const FILES_TO_CACHE = [
 ];
 
 const CACHE_NAME = 'static-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 //install event listener
 self.addEventListener('install', (event) => {
