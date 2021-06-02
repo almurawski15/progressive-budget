16fdb7236f38748b5c4efea36afaee3773050771
Alexandra Murawski <almurawski15@gmail.com>
2021-06-01 23:19:17 -0400

    connecting to mongoatlas

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
